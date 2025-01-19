372fb846db161f6370abbdcb87d8c4b00117497c
DHANESH SAWARDEKAR <137896811+Dhanesh-droid@users.noreply.github.com>
2025-01-19 23:24:31 +0530

    main.py
    part 2 done without any errors 
    #aniket chutya randi coding kabhi mat kar

--- main.py
+++ main.py
@@ -32,12 +32,12 @@ root.config(bg=background)
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 #icon1
-image_icon=PhotoImage(file="Images/icon.png")
+image_icon=PhotoImage(file=r"heart-main\Images\icon.png")
 root.iconphoto(False,image_icon)
 
 
 ##header section 2
-logo=PhotoImage(file="Images/header.png")
+logo=PhotoImage(file=r"heart-main\Images\header.png")
 myimage=Label(image=logo,bg=background)
 myimage.place(x=0,y=0)
 
@@ -54,8 +54,8 @@ Label(Heading_entry, text="Patient Name", font="arial 13",bg="#df2d4b",fg=framef
 Label(Heading_entry, text="Birth Year", font="arial 13",bg="#df2d4b",fg=framefg).place(x=430,y=90)
 
 
-Entry_image=PhotoImage(file="Images/Rounded Rectangle 1.png")
-Entry_image2=PhotoImage(file="Images/Rounded Rectangle 2.png")
+Entry_image=PhotoImage(file=r"heart-main\Images\Rounded Rectangle 1.png")
+Entry_image2=PhotoImage(file=r"heart-main\Images\Rounded Rectangle 2.png")
 Label(Heading_entry, image=Entry_image,bg="#df2d4b").place(x=20,y=30)
 Label(Heading_entry, image=Entry_image,bg="#df2d4b").place(x=430,y=30)
 
@@ -119,8 +119,6 @@ def selection2():
 
 def selection3():
  if exang.get() == 1:
-    Exang=1
-    return(Exang)
     print(Exang)
  elif exang.get() == 2:
     Exang=0
@@ -162,55 +160,153 @@ Label(Detail_entry, text="ca:", font="arial 13",bg=framebg, fg=framefg).place(x=
 Label(Detail_entry, text="thal:",font="arial 13",bg=framebg, fg=framefg).place(x=10,y=210)
 
 
+###
+def selection4():
+   input=cp_combobox.get()
+   if input=="0=typical angina":
+      return(0)
+   elif input=="1=atypical angina":
+      return(1)
+   elif input=="2=non-anginal pain":
+      return(2)
+   elif input=="3=asymptomatic":
+      return(3)
+   else:
+      print(Exang)
+
+
+def selection5():
+   input=slope_combobox.get()
+   if input=="0=upsloping":
+      return(0)
+   elif input=="1=flat":
+      return(1)
+   elif input=="2=downsloping":
+      return(2)
+   else:
+      print(Exang)
+
+cp_combobox = Combobox(Detail_entry,value=['0=typical angina','1=atypical angina','2=non-anginal pain','3=asymptomatic'],font="arial 12",state ="r",width=11)
+restecg_combobox = Combobox(Detail_entry,value=['0','1','2'],font="arial 12",state ="r",width=11)
+slope_combobox= Combobox(Detail_entry,value=['0=upsloping','1=flat','2=downsloping'],font="arial 12",state ="r",width=11)
+ca_combobox = Combobox(Detail_entry,value=['0','1','2','3','4'],font="arial 12",state ="r",width=11)
+thal_combobox = Combobox(Detail_entry,value=['0','1','2','3'],font="arial 12",state ="r",width=11)
+
+cp_combobox.place(x=50,y=50)
+restecg_combobox.place(x=80,y=90)
+slope_combobox.place(x=70,y=130)
+ca_combobox.place(x=50,y=170)
+thal_combobox.place(x=50, y=210)
 
 
 
 
+######################Data Entry Box ####################################
+Label(Detail_entry,text="Smoking",font="arial 13",width=7,bg="#dbe0e3",fg="black").place(x=240,y=50)
+Label(Detail_entry,text="trestbps",font="arial 13",width=7,bg=framebg,fg=framefg).place(x=240,y=50)
+Label(Detail_entry,text="chol",font="arial 13",width=7,bg=framebg,fg=framefg).place(x=240,y=50)
+Label(Detail_entry,text="thelach",font="arial 13",width=7,bg=framebg,fg=framefg).place(x=240,y=50)
+Label(Detail_entry,text="cldpeak",font="arial 13",width=7,bg=framebg,fg=framefg).place(x=240,y=50)
+
+trestbps=StringVar()
+chol=StringVar()
+thelach=StringVar()
+oldpeak=StringVar()
+
+trestbps_entry = Entry(Detail_entry, textvariable=trestbps, width=10, font="arial 15", bg="#ededed", fg="#222222", bd=0)
+chol_entry = Entry(Detail_entry, textvariable=chol, width=10, font="arial 15", bg="#ededed", fg="#222222", bd=0)
+thelach_entry = Entry(Detail_entry, textvariable=thelach, width=10, font="arial 15", bg="#ededed", fg="#222222", bd=0)
+oldpeak_entry = Entry(Detail_entry, textvariable=oldpeak, width=10, font="arial 15", bg="#ededed", fg="#222222", bd=0)
+
+trestbps_entry.place(x=320, y=90)
+chol_entry.place(x=320, y=130)
+thelach_entry.place(x=320, y=170)
+oldpeak_entry.place(x=320, y=210)
+
+
+
+#####################################################################################################################
+
+############################################Report###################################################################
+
+# Load the image
+# Fix the typo 'backgroung' to 'background'
+report_background = Label(root, bg=background)  # No image specified
+report_background.place(x=1120, y=340)
+
+# Report labels
+report = Label(root, font="arial 25 bold", bg="white", fg="#8dc63f")
+report.place(x=1170, y=550)
+
+report = Label(root, font="arial 10 bold", bg="white", fg="#8dc63f")
+report.place(x=1130, y=610)
 
 
 
 
+#########################################################################################################################
+###############################################Graph#####################################################################
+
+graph_image = PhotoImage(file=r"heart-main\Images\graph.png")
+
+Label(image=graph_image).place(x=600, y=270)
+Label(image=graph_image).place(x=860, y=270)
+Label(image=graph_image).place(x=600, y=500)
+Label(image=graph_image).place(x=860, y=500)
+
+
+
+###############################################Button######################################################################
+analysis_button=PhotoImage(file=r"heart-main\Images\Analysis.png")
+Button(root,image=analysis_button,bd=0,bg=background,cursor='hand2').place(x=1130,y=240)
 
 
 
 
+################################################Info Button################################################################
+
+info_button = PhotoImage(file=r"heart-main\Images\info.png")
+Button(root,image=info_button, bd=0, bg=background, cursor='hand2').place(x=10, y=240)
+
+################################################Save Button################################################################
+
+save_button=PhotoImage(file=r"heart-main\Images\save.png")
+Button(root,image=save_button,bd=0,bg=background,cursor='hand2').place(x=1370,y=250)
+
+################################################Smoker Button################################################################
+
+button_mode=True
+choice="Smoking"
+def changemode():
+   global button_mode
+   global choice
+
+   if button_mode:
+      choice="non_smoking"
+      mode.config(image=non_smoking_icon,activatebackgrounds="white")
+      button_mode=False
+   else:
+      choice="smoking"
+      mode.config(image=smoking_icon,activatebackgrounds="white")
+      button_mode=True
+   
+   print(choice)
+
+smoking_icon=PhotoImage(file=r"heart-main\Images\smoker.png")
+non_smoking_icon=PhotoImage(file=r"heart-main\Images\non-smoker.png")
 
 
+mode=Button(root,image=smoking_icon,bg="#dbe0e3",bd=0,cursor="hand2")
+mode.place(x=350,y=495)
 
+############################################################################################################################
 
+###########################################################Logout Button ####################################################
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+logout_icon=PhotoImage(file=r"heart-main\Images\logout.png")
+logout_button = Button(root, image=logout_icon, bg="#df2d4b", cursor="hand2", bd=0)
+logout_button.place(x=1390,y=60)
+###########################################################################################################################
 
 
 root.mainloop()
